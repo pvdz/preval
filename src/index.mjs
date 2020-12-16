b5b3c0ee97c710c1e704bd824f34171e981110fe
Peter van der Zee <github-public@qfox.nl>
2020-12-17 00:27:42 +0100

    Refactor

--- src/index.mjs
+++ src/index.mjs
@@ -31,22 +31,21 @@ export function preval({ entryPointFile, stdio, resolve, req }) {
           obj.usages.forEach(({ parent, prop, index }) => {
             // Cannot replace the declaration itself. But can eliminate it
             if (parent.type === 'VariableDeclarator' && prop !== 'init') {
-              // TODO: Eliminate this declaration...
-              log('TODO: eliminate var declarator');
+              return; // Ignore, phase2 will take care of this
+            }
+
+            log('Replacing a usage of `' + uniqueName + '` with a literal');
+            if (index >= 0) {
+              if (parent[prop][index] !== update) {
+                log('- actually replacing', parent.type + '[' + prop + '][' + index + '] with a', update.type);
+                parent[prop][index] = update;
+                changed = true;
+              }
             } else {
-              log('Replacing a usage of `' + uniqueName + '` with a literal');
-              if (index >= 0) {
-                if (parent[prop][index] !== update) {
-                  log('- actually replacing', parent.type + '[' + prop + '][' + index + '] with a', update.type);
-                  parent[prop][index] = update;
-                  changed = true;
-                }
-              } else {
-                if (parent[prop] !== update) {
-                  log('- actually replacing', parent.type + '[' + prop + '] with a', update.type, '->', index);
-                  parent[prop] = update;
-                  changed = true;
-                }
+              if (parent[prop] !== update) {
+                log('- actually replacing', parent.type + '[' + prop + '] with a', update.type, '->', index);
+                parent[prop] = update;
+                changed = true;
               }
             }
           });

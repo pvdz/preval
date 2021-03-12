89a320a474c8580e26970e6df482370e23cd78ba
Peter van der Zee <github-public@qfox.nl>
2021-03-12 20:53:17 +0100

    Abstract simple function inlining stuff

--- src/reduce_static/phase2.mjs
+++ src/reduce_static/phase2.mjs
@@ -6,6 +6,7 @@ import { pruneTrampolineFunctions } from './phase2trampoline.mjs';
 import { pruneExcessiveParams } from './phase2exparam.mjs';
 import { inlineConstants } from './phase2inlineconstants.mjs';
 import { promoteVars } from './phase2promotevars.mjs';
+import { inlineSimpleFuncCalls } from './phase2simplefuncs.mjs';
 
 let VERBOSE_TRACING = true;
 
@@ -105,68 +106,8 @@ export function phase2(program, fdata, resolve, req, verbose = VERBOSE_TRACING) 
   const promoted = promoteVars(fdata);
   if (promoted) return promoted;
 
-  group('\n\n\nChecking for func calls that can be inlined');
-  let inlinedFuncCount = 0;
-  fdata.globallyUniqueNamingRegistry.forEach((meta, name) => {
-    if (meta.isConstant && meta.constValueRef) {
-      ASSERT(meta.writes.length === 1);
-      const funcNode = meta.constValueRef.node;
-      if ((funcNode.type === 'FunctionExpression' || funcNode.type === 'ArrowFunctionExpression') && funcNode.$p.inlineMe) {
-        meta.reads.forEach((read) => {
-          if (read.parentNode.type === 'CallExpression' && read.parentProp === 'callee') {
-            // This read was a call to the function
-            if (funcNode.$p.inlineMe === 'single return with primitive') {
-              rule('Function that only returns primitive must be inlined');
-              example('function f() { return 5; } f();', '5;');
-              before(read.node, read.parentNode);
-
-              if (read.grandIndex >= 0) {
-                read.grandNode[read.grandProp][read.grandIndex] = AST.cloneSimple(funcNode.body.body[0].argument);
-              } else {
-                read.grandNode[read.grandProp] = AST.cloneSimple(funcNode.body.body[0].argument);
-              }
-              ++inlinedFuncCount;
-
-              after(read.parentNode);
-            } else if (funcNode.$p.inlineMe === 'double with primitive') {
-              rule('Function that returns local primitive should be inlined');
-              example('function f() { const x = undefined; return x; } f();', 'undefined;');
-              before(read.node, read.parentNode);
-
-              if (read.grandIndex >= 0) {
-                read.grandNode[read.grandProp][read.grandIndex] = funcNode.body.body[0].declarations[0].init;
-              } else {
-                read.grandNode[read.grandProp] = funcNode.body.body[0].declarations[0].init;
-              }
-              ++inlinedFuncCount;
-
-              after(read.parentNode);
-            } else if (funcNode.$p.inlineMe === 'double with array with primitives') {
-              rule('Function that returns array literal with only primitives should be inlined');
-              example('function f() { const arr = [1, 2]; return arr; } f();', '[1, 2];');
-              before(read.node, read.parentNode);
-
-              if (read.grandIndex >= 0) {
-                read.grandNode[read.grandProp][read.grandIndex] = funcNode.body.body[0].declarations[0].init;
-              } else {
-                read.grandNode[read.grandProp] = funcNode.body.body[0].declarations[0].init;
-              }
-              ++inlinedFuncCount;
-
-              after(read.parentNode);
-            }
-          }
-        });
-      }
-    }
-  });
-  groupEnd();
-  log('Inlined', inlinedFuncCount, 'function calls.');
-  if (inlinedFuncCount > 0) {
-    log('Restarting from phase1 to fix up read/write registry\n\n\n\n\n\n');
-    groupEnd();
-    return 'phase1';
-  }
+  const simpled = inlineSimpleFuncCalls(fdata);
+  if (simpled) return simpled;
 
   // The read/write data should still be in tact
 
---
+++ src/reduce_static/phase2simplefuncs.mjs
@@ -0,0 +1,88 @@
+import {
+  log,
+  group,
+  groupEnd,
+  ASSERT,
+  BLUE,
+  RED,
+  RESET,
+  tmat,
+  fmat,
+  TRIBE,
+  PURPLE,
+  DIM,
+  YELLOW,
+  rule,
+  example,
+  before,
+  source,
+  after,
+} from '../utils.mjs';
+import * as AST from '../ast.mjs';
+import { createFreshVar, createWriteRef } from '../bindings.mjs';
+import { VERBOSE_TRACING } from '../constants.mjs';
+
+export function inlineSimpleFuncCalls(fdata) {
+  group('\n\n\nChecking for func calls that can be inlined');
+  let inlinedFuncCount = 0;
+  fdata.globallyUniqueNamingRegistry.forEach((meta, name) => {
+    if (meta.isConstant && meta.constValueRef) {
+      ASSERT(meta.writes.length === 1);
+      const funcNode = meta.constValueRef.node;
+      if ((funcNode.type === 'FunctionExpression' || funcNode.type === 'ArrowFunctionExpression') && funcNode.$p.inlineMe) {
+        meta.reads.forEach((read) => {
+          if (read.parentNode.type === 'CallExpression' && read.parentProp === 'callee') {
+            // This read was a call to the function
+            if (funcNode.$p.inlineMe === 'single return with primitive') {
+              rule('Function that only returns primitive must be inlined');
+              example('function f() { return 5; } f();', '5;');
+              before(read.node, read.parentNode);
+
+              if (read.grandIndex >= 0) {
+                read.grandNode[read.grandProp][read.grandIndex] = AST.cloneSimple(funcNode.body.body[0].argument);
+              } else {
+                read.grandNode[read.grandProp] = AST.cloneSimple(funcNode.body.body[0].argument);
+              }
+              ++inlinedFuncCount;
+
+              after(read.parentNode);
+            } else if (funcNode.$p.inlineMe === 'double with primitive') {
+              rule('Function that returns local primitive should be inlined');
+              example('function f() { const x = undefined; return x; } f();', 'undefined;');
+              before(read.node, read.parentNode);
+
+              if (read.grandIndex >= 0) {
+                read.grandNode[read.grandProp][read.grandIndex] = funcNode.body.body[0].declarations[0].init;
+              } else {
+                read.grandNode[read.grandProp] = funcNode.body.body[0].declarations[0].init;
+              }
+              ++inlinedFuncCount;
+
+              after(read.parentNode);
+            } else if (funcNode.$p.inlineMe === 'double with array with primitives') {
+              rule('Function that returns array literal with only primitives should be inlined');
+              example('function f() { const arr = [1, 2]; return arr; } f();', '[1, 2];');
+              before(read.node, read.parentNode);
+
+              if (read.grandIndex >= 0) {
+                read.grandNode[read.grandProp][read.grandIndex] = funcNode.body.body[0].declarations[0].init;
+              } else {
+                read.grandNode[read.grandProp] = funcNode.body.body[0].declarations[0].init;
+              }
+              ++inlinedFuncCount;
+
+              after(read.parentNode);
+            }
+          }
+        });
+      }
+    }
+  });
+  groupEnd();
+  log('Inlined', inlinedFuncCount, 'function calls.');
+  if (inlinedFuncCount > 0) {
+    log('Restarting from phase1 to fix up read/write registry\n\n\n\n\n\n');
+    groupEnd();
+    return 'phase1';
+  }
+}

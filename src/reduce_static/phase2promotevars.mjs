41add72feeb8c50d5cf3558dce433120e757d9a5
Peter van der Zee <github-public@qfox.nl>
2021-03-30 01:39:05 +0200

    Cleanup the SSA code

--- src/reduce_static/phase2promotevars.mjs
+++ src/reduce_static/phase2promotevars.mjs
@@ -3,36 +3,25 @@ import * as AST from '../ast.mjs';
 import { createFreshVar, createWriteRef } from '../bindings.mjs';
 
 export function promoteVars(fdata) {
-  group('\n\n\nChecking for promotable vars\n');
+  group('\n\n\nChecking for vars to SSA\n');
   const r = _promoteVars(fdata);
   groupEnd();
   return r;
 }
 function _promoteVars(fdata) {
   let promoted = 0;
-  fdata.globallyUniqueNamingRegistry.forEach((meta, name) => {
+  // Shallow clone to prevent mutations to the registry from breaking because their read/write refs did not go through phase1
+  new Map(fdata.globallyUniqueNamingRegistry).forEach((meta, name) => {
     if (meta.isBuiltin) return;
     if (meta.isImplicitGlobal) return;
 
-    vgroup('- `' + name + '`');
-    vlog(
-      '-',
-      meta.reads.length,
-      'reads and',
-      meta.writes.length,
-      'writes',
-      meta.isConstant ? '(a constant)' : '(not a constant)',
-      meta.constValueRef?.node?.type ?? '',
-    );
+    vgroup('- `' + name + '`:', meta.constValueRef?.node?.type ?? '<no decl>', ', reads:', meta.reads.length, ', writes:', meta.writes.length);
 
-    // Check if all usages of the binding is consolidated to one scope
-    const writeScopes = new Set();
-    meta.writes.forEach((write) => writeScopes.add(write.scope));
-    const readScopes = new Set();
-    meta.reads.forEach((write) => readScopes.add(write.scope));
-
-    ASSERT(meta.writes.length > 0, 'all bindings must have at least some writes...');
-    const declData = meta.writes[0].decl;
+    if (meta.writes[0].kind !== 'var') {
+      vlog('The first write was not a decl, bailing');
+      vgroupEnd();
+      return;
+    }
 
     // If a binding start with a var decl as first write (prevents func decl closure problem) and the next
     // usage is an assignment write then the first decl can be dropped (if it has an init then that becomes
@@ -54,159 +43,123 @@ function _promoteVars(fdata) {
     //   - there are no prior reads in the same or an even deeper loop
     //   - all future reads are in the same scope
 
-    vlog('Starts with decl?', !!declData);
+    vlog('The binding `' + name + '` has a var decl. Analyzing usages (', meta.reads.length, 'reads and', meta.writes.length, 'writes).');
 
-    // "Is this binding defined through a var decl?" -- prevents forx, func decl closures, implicit globals, and TDZ cases.
-    if (declData) {
-      vlog('The binding `' + name + '` has a var decl. Analyzing usages (', meta.reads.length, 'reads and', meta.writes.length, 'writes).');
+    const rwOrder = [...meta.reads, ...meta.writes].sort(({ rwCounter: a }, { rwCounter: b }) => (a < b ? -1 : a > b ? 1 : 0));
+    vlog('rwOrder:', [rwOrder.map((o) => o.action).join(', ')]);
 
-      const rwOrder = [...meta.reads, ...meta.writes].sort(({ rwCounter: a }, { rwCounter: b }) => (a < b ? -1 : a > b ? 1 : 0));
-      vlog('rwOrder:', [rwOrder.map((o) => o.action).join(', ')]);
-      // Note: We asserted that the first write is a var decl, but a closure in func decl may still put a read as the first source ref.
-      if (rwOrder[0].decl) {
-        ASSERT(rwOrder[0].action === 'write', 'the .decl check subsumes this, right?');
-        //vlog('The initial binding:');
-        //source(rwOrder[0].parentNode);
+    // Note: We asserted that the first write is a var decl, but a closure in func decl may still put a read as the first source ref.
+    if (rwOrder[0].kind !== 'var') {
+      vlog('There was a read in the source before the decl, bailing');
+      vgroupEnd();
+      return;
+    }
 
-        ASSERT(
-          rwOrder.slice(1).every((rw) => (!rw.decl ? true : !!void console.dir(rw, { depth: null }))),
-          'a binding should have no more than one var decl after normalization',
-          name,
-          rwOrder,
-        );
+    // Bail as soon as we find a read/write in a different scope. In that case we have a TDZ or closure
+    // and source-order-"future" read/writes can not (easily) statically be guaranteed to be safe.
+    const bindingScope = rwOrder[0].scope;
 
-        // Bail as soon as we find a read/write in a different scope. In that case we have a TDZ or closure
-        // and source-order-"future" read/writes can not (easily) statically be guaranteed to be safe.
-        const bindingScope = rwOrder[0].scope;
+    // Note: rwOrder may have holes after the loop but during the loop
+    for (let i = 1; i < rwOrder.length; ++i) {
+      const rwRef = rwOrder[i];
 
-        // Note: rwOrder may have holes after the loop but during the loop, a and b should not be null
-        for (let i = 1; i < rwOrder.length; ++i) {
-          const a = rwOrder[i - 1];
-          const b = rwOrder[i];
+      vlog('- rwOrder[' + i + '] =', rwRef.kind);
 
-          vlog('- rwOrder[' + i + '] =', b.action);
-          //source(b.parentNode);
+      if (rwRef.scope !== bindingScope) {
+        // TODO: I think there are situations where we can still safely support this case
+        vlog('Found a read/write in a different scope. Bailing as we cannot guarantee the remaining read/writes.');
+        break;
+      }
 
-          if (b.scope !== bindingScope) {
-            // TODO: I think there are situations where we can still safely support this case
-            vlog('Found a read/write in a different scope. Bailing as we cannot guarantee the remaining read/writes.');
-            break;
-          }
+      if (rwRef.action === 'write') {
+        // To SSA;
+        // - it must be an assignment
+        // - all future reads must reach this write
+        // - if the current write is inside a loop (while, for-x)
+        //   - all future reads must not be outside the current loop
+        //   - all future reads must be in the same scope
+        //   - all previous reads must be before the current loop
 
-          if (b.action === 'write') {
-            // Must be
-            // - an assignment
-            // - all future reads must reach this write
-            // - if the current write is inside a loop (while, for-x)
-            //   - all future reads must not be outside the current loop
-            //   - all future reads must be in the same scope
-            //   - all previous reads must be before the current loop
+        vlog('Is the write an assign?', rwRef.kind);
 
-            vlog('Is the write an assign?', !!b.assign);
+        // Verify that the write is an assign that happens in the same scope because we must ignore closures for now
+        if (rwRef.kind === 'assign') {
+          // Must verify that all remaining usages can reach this write
 
-            // Verify that the write is an assign that happens in the same scope because we must ignore closures for now
-            ASSERT(!b.decl, 'a decl must be the first write and a and b were both writes so b cannot be the var decl');
-            if (b.assign) {
-              // Must verify that all remaining usages can reach this write
-
-              let loopId = b.innerLoop;
-              vlog('Write inside a loop?', loopId);
-              let canSSA = true;
-              if (loopId) {
-                vlog('Checking if any previous read can reach this write (it is bad if one does)');
-                // All previous reads must not be able to reach this assign (because that implies they're part of the loop)
-                for (let j = 0; j < i; ++j) {
-                  const c = rwOrder[j];
-                  if (c.action === 'read') {
-                    vlog(
-                      '-',
-                      j,
-                      ':',
-                      c.action,
-                      ', same loop:',
-                      loopId === c.innerLoop,
-                      ', can reach:',
-                      c.blockChain.startsWith(b.blockChain),
-                    );
-                    if (loopId === c.innerLoop) {
-                      vlog('At least one previous ref is in the same loop so this we can not SSA');
-                      canSSA = false;
-                      break;
-                    }
-                    if (c.blockChain.startsWith(b.blockChain)) {
-                      vlog('At least one previous ref can reach this write so this we can not SSA');
-                      canSSA = false;
-                      break;
-                    }
-                  }
+          let loopId = rwRef.innerLoop;
+          vlog('Write inside a loop?', loopId);
+          let canSSA = true;
+          if (loopId) {
+            vlog('Checking if any previous read can reach this write (it is bad if one does)');
+            // All previous reads must not be able to reach this assign (because that implies they're part of the loop)
+            for (let j = 0; j < i; ++j) {
+              const c = rwOrder[j];
+              if (c.action === 'read') {
+                vlog(
+                  '-',
+                  j,
+                  ':',
+                  c.action,
+                  ', same loop:',
+                  loopId === c.innerLoop,
+                  ', can reach:',
+                  c.blockChain.startsWith(rwRef.blockChain),
+                );
+                if (loopId === c.innerLoop) {
+                  vlog('At least one previous ref is in the same loop so this we can not SSA');
+                  canSSA = false;
+                  break;
                 }
-              }
-              if (canSSA) {
-                vlog('Checking if all future reads can reach this write (good) and that they are in the same scope (good)');
-                for (let j = i + 1; j < rwOrder.length; ++j) {
-                  const c = rwOrder[j];
-                  vlog('-', j, ': can reach:', c.blockChain.startsWith(b.blockChain), ', same scope:', b.scope === c.scope);
-                  // Closure? Only relevant if assignment is in a loop.
-                  if (loopId && b.scope !== c.scope) {
-                    vlog('At least one future read/write was in a different scope');
-                    canSSA = false;
-                    break;
-                  }
-                  // A usage c can reach another usage b if the blockChain of b is a prefix of the blockchain of c
-                  if (!c.blockChain.startsWith(b.blockChain)) {
-                    vlog('At least one future read/write can not reach this assignment');
-                    canSSA = false;
-                    break;
-                  }
+                if (c.blockChain.startsWith(rwRef.blockChain)) {
+                  vlog('At least one previous ref can reach this write so this we can not SSA');
+                  canSSA = false;
+                  break;
                 }
-              }
-
-              if (canSSA) {
-                vlog('Applying SSA now');
-                rule('A redundant assign where remaining usages can all reach it must be SSA-ed');
-                example('let x = 10; x = 20; f(x);', 'let x = 10; let x2 = 20; f(x2);');
-                before(b.node);
-
-                const newName = createFreshVar(name.startsWith('tmpSSA_') ? name : 'tmpSSA_' + name, fdata);
-                const newMeta = fdata.globallyUniqueNamingRegistry.get(newName);
-                b.node.name = newName;
-
-                // Convert the assignment node represented by `b` into a let decl
-
-                meta.writes.splice(meta.writes.indexOf(b), 1);
-                newMeta.writes.push(b);
-
-                // Replace the assignment with a var decl of the same kind
-                const { assignParent, assignProp, assignIndex } = b.assign;
-                const assignExpr = assignIndex >= 0 ? assignParent[assignProp][assignIndex] : assignParent[assignProp];
-                ASSERT(b.parentNode.type === 'AssignmentExpression', 'if not then indexes changed?', assignExpr);
-                ASSERT(b.parentNode.left === b.node, 'should still be node');
-                const rhs = b.parentNode.right;
-                const newNode = AST.variableDeclaration(b.node, rhs, 'let'); // it SHOULD be fine to change this to a let...?
-                if (assignIndex >= 0) assignParent[assignProp][assignIndex] = newNode;
-                else assignParent[assignProp] = newNode;
-
-                for (let j = i + 1; j < rwOrder.length; ++j) {
-                  const c = rwOrder[j];
-
-                  if (c.action === 'write') {
-                    meta.writes.splice(meta.writes.indexOf(c), 1);
-                    newMeta.writes.push(c);
-                  } else {
-                    meta.reads.splice(meta.reads.indexOf(c), 1);
-                    newMeta.reads.push(c);
-                  }
-
-                  c.node.name = newName;
-                }
-
-                after(b.node);
-                ++promoted;
-                //endit
-              } else {
-                vlog('At least one subsequent usage can not reach this write so we can not easily SSA here');
               }
             }
+          }
+          if (canSSA) {
+            vlog('Checking if all future reads can reach this write (good) and that they are in the same scope (good)');
+            for (let j = i + 1; j < rwOrder.length; ++j) {
+              const c = rwOrder[j];
+              vlog('-', j, ': can reach:', c.blockChain.startsWith(rwRef.blockChain), ', same scope:', rwRef.scope === c.scope);
+              // Closure? Only relevant if assignment is in a loop.
+              if (loopId && rwRef.scope !== c.scope) {
+                vlog('At least one future read/write was in a different scope');
+                canSSA = false;
+                break;
+              }
+              // A usage c can reach another usage b if the blockChain of b is a prefix of the blockchain of c
+              if (!c.blockChain.startsWith(rwRef.blockChain)) {
+                vlog('At least one future read/write can not reach this assignment');
+                canSSA = false;
+                break;
+              }
+            }
+          }
+
+          if (canSSA) {
+            vlog('Applying SSA now');
+            rule('A redundant assign where remaining usages can all reach it must be SSA-ed');
+            example('let x = 10; x = 20; f(x);', 'let x = 10; let x2 = 20; f(x2);');
+            before(rwRef.node);
+
+            const newName = createFreshVar(name.startsWith('tmpSSA_') ? name : 'tmpSSA_' + name, fdata);
+
+            // Replace the assignment with a var decl of the same kind
+            const rhs = rwRef.parentNode.right;
+            const newNode = AST.variableDeclaration(rwRef.node, rhs, 'let'); // it SHOULD be fine to change this to a let...?
+            rwRef.blockBody[rwRef.blockIndex] = newNode;
+
+            for (let j = i; j < rwOrder.length; ++j) {
+              rwOrder[j].node.name = newName;
+            }
+
+            after(rwRef.node);
+            ++promoted;
+            //endit
+          } else {
+            vlog('At least one subsequent usage can not reach this write so we can not easily SSA here');
           }
         }
       }
@@ -215,9 +168,10 @@ function _promoteVars(fdata) {
     vgroupEnd();
   });
 
-  log('\nPromoted', promoted, 'bindings to a constant');
   if (promoted) {
-    log('Restarting from phase1 to fix up read/write registry\n\n\n\n\n\n');
+    log('Bindings SSAd:', promoted, '. Restarting from phase1 to fix up read/write registry');
     return 'phase1';
   }
+
+  log('Bindings SSAd: 0.');
 }

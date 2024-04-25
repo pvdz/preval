a27996e96459e6eefc05038f3058438c9a9e1686
Peter van der Zee <github-public@qfox.nl>
2024-04-25 22:21:08 +0200

    Simplify. Maybe. I guess

--- src/reduce_static/_phase2.mjs
+++ src/reduce_static/_phase2.mjs
@@ -185,266 +185,99 @@ function _phase2(program, fdata, resolve, req, options = {}) {
     });
   });
 
-  const coerced = coercials(fdata);
-  if (coerced) return coerced;
-
-  const bounds = resolveBoundValueSet(fdata);
-  if (bounds) return bounds;
-
-  const am = arr_mutation(fdata); // Do early because it can be expensive with many writes
-  if (am) return am;
-
-  const throwers = findThrowers(fdata);
-  if (throwers) return throwers;
-
-  const tdzd = singleScopeTdz(fdata);
-  if (tdzd) return tdzd;
-
-  const consts = constAssigns(fdata);
-  if (consts) return consts;
-
-  const ag = aliasedGlobals(fdata);
-  if (ag) return ag;
-
-  const dc = dotCall(fdata);
-  if (dc) return dc;
-
-  const moved = letHoisting(fdata);
-  if (moved) return moved;
-
-  const extended = ifTailExtending(fdata);
-  if (extended) return extended;
-
-  const moved2 = assignHoisting(fdata);
-  if (moved2) return moved2;
-
-  const flipped = ifFlipping(fdata);
-  if (flipped) return flipped;
-
-  const staticLetsInlined = staticLets(fdata);
-  if (staticLetsInlined) return staticLetsInlined;
-
-  const emptyFuncs = pruneEmptyFunctions(fdata);
-  if (emptyFuncs) return emptyFuncs;
-
-  const trampFuncs = pruneTrampolineFunctions(fdata);
-  if (trampFuncs) return trampFuncs;
-
-  const inlinedConstants = inlineConstants(fdata);
-  if (inlinedConstants) return inlinedConstants;
-
-  const dealiased = dealiasing(fdata);
-  if (dealiased) return dealiased;
-
-  const singleSSA = singleScopeSSA(fdata);
-  if (singleSSA) return singleSSA;
-
-  const multiSSA = multiScopeSSA(fdata);
-  if (multiSSA) return multiSSA;
-
-  const prunedParams = pruneExcessiveParams(fdata);
-  if (prunedParams) return prunedParams;
-
-  const ea = excessiveArgs(fdata);
-  if (ea) return ea;
-
-  const singled = inlineOneTimeFunctions(fdata);
-  if (singled) return singled;
-
-  const simpled = inlineSimpleFuncCalls(fdata);
-  if (simpled) return simpled;
-
-  const globaled = funcScopePromo(fdata);
-  if (globaled) return globaled;
-
-  const deduped = dedupeBranchedReturns(fdata);
-  if (deduped) return deduped;
-
-  const commoned = inlineCommonReturns(fdata);
-  if (commoned) return commoned;
-
-  const dropped = dropUnusedReturns(fdata);
-  if (dropped) return dropped;
-
-  const ifelsed = ifelseifelse(fdata);
-  if (ifelsed) return ifelsed;
-
-  const ifcallifed = ifCallIf(fdata);
-  if (ifcallifed) return ifcallifed;
-
-  const arrrrred = arrrrrr(fdata);
-  if (arrrrred) return arrrrred;
-
-  const propped = objlitPropAccess(fdata);
-  if (propped) return propped;
-
-  const bitset = bitSetTests(fdata);
-  if (bitset) return bitset;
-
-  const callsHoisted = ifUpdateCall(fdata);
-  if (callsHoisted) return callsHoisted;
-
-  const lengthed = inlineArgLen(fdata);
-  if (lengthed) return lengthed;
-
-  const identicals = inlineIdenticalParam(fdata);
-  if (identicals) return identicals;
-
-  const unclosured = returnClosure(fdata);
-  if (unclosured) return unclosured;
-
-  const retargs = returnArg(fdata);
-  if (retargs) return retargs;
-
-  const whiled = constWhileTest(fdata);
-  if (whiled) return whiled;
-
-  const typed = typeTrackedTricks(fdata);
-  if (typed) return typed;
-
-  const spread = arrSpreads(fdata);
-  if (spread) return spread;
-
-  const condTypings = conditionalTyping(fdata);
-  if (condTypings) return condTypings;
-
-  const testsBooled = ifTestBool(fdata);
-  if (testsBooled) return testsBooled;
-
-  const ifsFolded = ifTestFolding(fdata);
-  if (ifsFolded) return ifsFolded;
-
-  const dualed = ifDualAssign(fdata);
-  if (dualed) return dualed;
-
-  const returnedParams = returnsParam(fdata);
-  if (returnedParams) return returnedParams;
-
-  const varsMoved = spylessVars(fdata);
-  if (varsMoved) return varsMoved;
-
-  const fused = stringFusing(fdata);
-  if (fused) return fused;
-
-  const ands = andCases(fdata);
-  if (ands) return ands;
-
-  const casted = globalCasting(fdata);
-  if (casted) return casted;
-
-  const binned = binExprStmt(fdata);
-  if (binned) return binned;
-
-  const looked = propertyLookups(fdata);
-  if (looked) return looked;
-
-  const leffed = letIfElse(fdata);
-  if (leffed) return leffed;
-
-  const rwrites = redundantWrites(fdata);
-  if (rwrites) return rwrites;
-
-  const redupped = ifReduceUp(fdata);
-  if (redupped) return redupped;
-
-  const orxors = orXor(fdata);
-  if (orxors) return orxors;
-
-  const comps = typedComparison(fdata);
-  if (comps) return comps;
-
-  const ebts = eqBang(fdata);
-  if (ebts) return ebts;
-
-  const ores = orOr(fdata);
-  if (ores) return ores;
-
-  const ants = andAnd(fdata);
-  if (ants) return ants;
-
-  const bc = branchConstantInlining(fdata);
-  if (bc) return bc;
-
-  const bt = boolTrampolines(fdata);
-  if (bt) return bt;
-
-  const rp = restParams(fdata);
-  if (rp) return rp;
-
-  const iaia = andIfAndIf(fdata);
-  if (iaia) return iaia;
-
-  const im = ifMerging(fdata);
-  if (im) return im;
-
-  const ifs = ifFalsySpread(fdata);
-  if (ifs) return ifs;
-
-  const tb = tailBreaking(fdata);
-  if (tb) return tb;
-
-  const fl = fakeLoops(fdata);
-  if (fl) return fl;
-
-  const uwt = unwindWhileWithTest(fdata, options.unrollLimit);
-  if (uwt) return uwt;
-
-  const uwc = unwindWhileWithCounter(fdata, options.unrollLimit);
-  if (uwc) return uwc;
-
-  const sao = staticArgOpOutlining(fdata);
-  if (sao) return sao;
-
-  const ful = functionLocks(fdata);
-  if (ful) return ful;
-
-  const ro = readOnce(fdata);
-  if (ro) return ro;
-
-  const to = testing_only(fdata);
-  if (to) return to;
-
-  const fp = functionSplitting(fdata);
-  if (fp) return fp;
-
-  const nt = noopTry(fdata);
-  if (nt) return nt;
-
-  const it = implicitThis(fdata, options.implicitThisIdent);
-  if (it) return it;
-
-  const ep = expandoSplitting(fdata);
-  if (ep) return ep;
-
-  const sac = selfAssignClosure(fdata);
-  if (sac) return sac;
-
-  const san = selfAssignNoop(fdata);
-  if (san) return san;
-
-  const la = letAliasing(fdata);
-  if (la) return la;
-
-  //const ltw = letTrueWhile(fdata);
-  //if (ltw) return ltw;
-
-  //const liwx = letIfElseFalseWhile(fdata);
-  //if (liwx) return liwx;
-
-  const ta = testingAlias(fdata);
-  if (ta) return ta;
-
-  const aii = aliasIfIf(fdata);
-  if (aii) return aii;
-
-  // This one should probably be lowest priority as it might blow up code...
-  const ulwt = unrollLoopWithTrue(fdata, options.unrollTrueLimit);
-  if (ulwt) return ulwt;
-
-
-  // This one is very invasive and expands the code. Needs more work.
-  // const duped = phasePrimitiveArgInlining(program, fdata, resolve, req, options.cloneLimit);
-  // if (duped) return duped;
+  return (
+    coercials(fdata) ||
+    resolveBoundValueSet(fdata) ||
+    // Do early because it can be expensive with many writes
+    arr_mutation(fdata) ||
+    findThrowers(fdata) ||
+    singleScopeTdz(fdata) ||
+    constAssigns(fdata) ||
+    aliasedGlobals(fdata) ||
+    dotCall(fdata) ||
+    letHoisting(fdata) ||
+    ifTailExtending(fdata) ||
+    assignHoisting(fdata) ||
+    ifFlipping(fdata) ||
+    staticLets(fdata) ||
+    pruneEmptyFunctions(fdata) ||
+    pruneTrampolineFunctions(fdata) ||
+    inlineConstants(fdata) ||
+    dealiasing(fdata) ||
+    singleScopeSSA(fdata) ||
+    multiScopeSSA(fdata) ||
+    pruneExcessiveParams(fdata) ||
+    excessiveArgs(fdata) ||
+    inlineOneTimeFunctions(fdata) ||
+    inlineSimpleFuncCalls(fdata) ||
+    funcScopePromo(fdata) ||
+    dedupeBranchedReturns(fdata) ||
+    inlineCommonReturns(fdata) ||
+    dropUnusedReturns(fdata) ||
+    ifelseifelse(fdata) ||
+    ifCallIf(fdata) ||
+    arrrrrr(fdata) ||
+    objlitPropAccess(fdata) ||
+    bitSetTests(fdata) ||
+    ifUpdateCall(fdata) ||
+    inlineArgLen(fdata) ||
+    inlineIdenticalParam(fdata) ||
+    returnClosure(fdata) ||
+    returnArg(fdata) ||
+    constWhileTest(fdata) ||
+    typeTrackedTricks(fdata) ||
+    arrSpreads(fdata) ||
+    conditionalTyping(fdata) ||
+    ifTestBool(fdata) ||
+    ifTestFolding(fdata) ||
+    ifDualAssign(fdata) ||
+    returnsParam(fdata) ||
+    spylessVars(fdata) ||
+    stringFusing(fdata) ||
+    andCases(fdata) ||
+    globalCasting(fdata) ||
+    binExprStmt(fdata) ||
+    propertyLookups(fdata) ||
+    letIfElse(fdata) ||
+    redundantWrites(fdata) ||
+    ifReduceUp(fdata) ||
+    orXor(fdata) ||
+    typedComparison(fdata) ||
+    eqBang(fdata) ||
+    orOr(fdata) ||
+    andAnd(fdata) ||
+    branchConstantInlining(fdata) ||
+    boolTrampolines(fdata) ||
+    restParams(fdata) ||
+    andIfAndIf(fdata) ||
+    ifMerging(fdata) ||
+    ifFalsySpread(fdata) ||
+    tailBreaking(fdata) ||
+    fakeLoops(fdata) ||
+    unwindWhileWithTest(fdata, options.unrollLimit) ||
+    unwindWhileWithCounter(fdata, options.unrollLimit) ||
+    staticArgOpOutlining(fdata) ||
+    functionLocks(fdata) ||
+    readOnce(fdata) ||
+    testing_only(fdata) ||
+    functionSplitting(fdata) ||
+    noopTry(fdata) ||
+    implicitThis(fdata, options.implicitThisIdent) ||
+    expandoSplitting(fdata) ||
+    selfAssignClosure(fdata) ||
+    selfAssignNoop(fdata) ||
+    letAliasing(fdata) ||
+    // letTrueWhile(fdata) ||
+    // letIfElseFalseWhile(fdata) ||
+    testingAlias(fdata) ||
+    aliasIfIf(fdata) ||
+    // This one should probably be lowest priority as it might blow up code...
+    unrollLoopWithTrue(fdata, options.unrollTrueLimit)
+
+
+    //// This one is very invasive and expands the code. Needs more work.
+    //phasePrimitiveArgInlining(program, fdata, resolve, req, options.cloneLimit) ||
+  );
 
   // The read/write data should still be in tact at this point
 }

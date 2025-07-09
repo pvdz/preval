5dfd635e9de41f6f8088d93c567ef93092b462d9
Peter van der Zee <github-public@qfox.nl>
2025-07-09 21:37:00 +0200

    Add --unroll0 and remove out/extra for the A shortcut

--- tests/p.sh
+++ tests/p.sh
@@ -80,7 +80,7 @@ Preval CLI Toolkit help:
  u               Run all test files and just write output
  U               Run all test files and force write output (ignores ASSERT failures)
  m               Run all tests and ask for update one-by-one
- A <path>        Alias for `rm -r out && mkdirp out && ./p F <path> --logto out --silent --time`
+ A <path>        Alias for `rm -r out/extra && mkdirp out && ./p F <path> --logto out --silent --time --unroll 0`
  fast            Omit many of the expression variation tests (ignores about 18k auto generated tests). Use -fast for the inverse.
 
  --log           Automatically write code after every phase executed to a log file. Defaults to .
@@ -157,8 +157,8 @@ Preval CLI Toolkit help:
       ;;
     A)
       # Force update all test files with their current output
-      rm -rf out
-      mkdir out
+      rm -rf out/extra
+      mkdir out || true
       ACTION='-F'
       shift
       ACTION_ARG=$1
@@ -166,6 +166,8 @@ Preval CLI Toolkit help:
       PARAM_LOGTO='out'
       PARAM_SILENT='--silent'
       PARAM_TIME='--time'
+      PARAM_UNROLL='--unroll'
+      PARAM_UNROLL_VALUE='0'
       ;;
 
     --log)
